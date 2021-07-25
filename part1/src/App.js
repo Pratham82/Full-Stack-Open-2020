699958ed64e5e9d8709a0239e710cccaa4d3dd0c
pratham82 <mali.prathamesh82@gmail.com>
2021-07-25 19:40:49 +0530

    Completed till part 1b

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,59 @@
 import React from 'react'
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
 
-  const Header = (props) => <h1>{props.course}</h1>
-  const Content = () => (
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+  const { name, parts } = course
+  const Header = ({ course }) => <h1>{name}</h1>
+  const Content = ({ parts }) => (
     <>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </>
   )
-  const Part = ({ part }) => <p>ddd{part}</p>
-  const Total = ({ marks }) => (
-    <p> Number of exercises {marks.reduce((a, b) => a + b)}</p>
+
+  const Part = ({ name, exercises }) => (
+    <p>
+      <b>Name:</b> {name}
+      <br />
+      <b>Exercises:</b> {exercises}
+    </p>
+  )
+  const Total = ({ parts }) => (
+    <>
+      <p>
+        Total exercises = {parts.reduce((acc, part) => part.exercises + acc, 0)}
+      </p>
+    </>
   )
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total marks={[exercises1, exercises2, exercises3]} />
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
